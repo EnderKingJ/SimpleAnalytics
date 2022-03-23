9f74762b1619033a3807d28f7f85083b4f891ccc
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-03-23 23:25:00 +0000

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,15 @@
+import Analytics from '../index.js';
+import http from 'node:http';
+import serve from 'node-static';
+
+var Serve = new serve.Server('./example');
+
+var server = http.createServer();
+
+server.on('request', (req, res) => {
+  if (!Analytics(req, res)) return false;
+
+  Serve.serve(req, res)
+})
+
+server.listen(8080)
